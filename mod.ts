ca9a21531227a335ea1314540df999fc772b1a06
1pedroalmeida <prvma.pedro@gmail.com>
2024-09-13 23:52:27 +0100

    better error messages

--- mod.ts
+++ mod.ts
@@ -2,76 +2,86 @@
 //TODO automatic transactions
 //TODO savings
 
-import { encode, decode, getFileData, getBalance, printBannerMsg, formatDate } from "./utils.ts";
+import {
+  decode,
+  encode,
+  formatDate,
+  getBalance,
+  getFileData,
+  printBannerMsg,
+} from "./utils.ts";
 
 const appdata = Deno.env.get("LOCALAPPDATA");
 const folder = appdata + "\\mon\\";
 const filePath = folder + ".mon";
 
 try {
-    Deno.statSync(filePath);
+  Deno.statSync(filePath);
 } catch (_err) {
-    try {
-        Deno.statSync(folder);
-    } catch (_err) {
-        await Deno.mkdir(folder);
-    }
+  try {
+    Deno.statSync(folder);
+  } catch (_err) {
+    await Deno.mkdir(folder);
+  }
 
-    await Deno.create(filePath);
-    Deno.writeTextFileSync(filePath, "");
+  await Deno.create(filePath);
+  Deno.writeTextFileSync(filePath, "");
 }
-
 
 let data = getFileData(filePath);
 let balance = getBalance(data);
 
-if(isNaN(balance)) {
-    Deno.stdout.writeSync(encode("error: .mon file is corrupted"));
-    Deno.exit(1);
+if (isNaN(balance)) {
+  Deno.stdout.writeSync(
+    encode("\u001b[31merror\u001b[0m: .mon file is corrupted"),
+  );
+  Deno.exit(1);
 }
 
 printBannerMsg(balance);
 
 while (true) {
-    const date = new Date();
-    const buf = new Uint8Array(1024);
+  const date = new Date();
+  const buf = new Uint8Array(1024);
 
-    Deno.stdout.writeSync(encode("> "));
+  Deno.stdout.writeSync(encode("> "));
 
-    const n = await Deno.stdin.read(buf);
+  const n = await Deno.stdin.read(buf);
 
-    if (n !== null) {
-        const input = decode(buf.subarray(0, n)).replace("\n", "").replace(
-            "\r",
-            "",
-        );
+  if (n !== null) {
+    const input = decode(buf.subarray(0, n)).replace("\n", "").replace(
+      "\r",
+      "",
+    );
 
-        if (["history", "h"].includes(input)) {
-            //TODO history
-            Deno.stdout.writeSync(encode(getFileData(filePath)));
-            continue;
-        }
+    if (["history", "h"].includes(input)) {
+      //TODO history
+      Deno.stdout.writeSync(encode(getFileData(filePath)));
+      continue;
+    }
 
-        if (["clear", "c"].includes(input)) {
-            Deno.writeTextFileSync(filePath, `[${formatDate(date)}]${balance}\n`);
-            printBannerMsg(balance, "History has been cleared");
-            continue;
-        }
+    if (["clear", "c"].includes(input)) {
+      Deno.writeTextFileSync(filePath, `[${formatDate(date)}]${balance}\n`);
+      printBannerMsg(balance, "History has been cleared");
+      continue;
+    }
 
-        if(["reset", "R"].includes(input)) {
-            Deno.writeTextFileSync(filePath, "");
-            printBannerMsg(0, "Account balance has been reset")
-            continue;
-        }
+    if (["reset", "R"].includes(input)) {
+      Deno.writeTextFileSync(filePath, "");
+      printBannerMsg(0, "Account balance has been reset");
+      continue;
+    }
 
-        if (["quit", "q"].includes(input)) {
-            Deno.stdout.writeSync(encode("\u001bc"));
+    if (["quit", "q"].includes(input)) {
+      Deno.stdout.writeSync(encode("\u001bc"));
 
       Deno.exit(0);
     }
 
     if (isNaN(Number(input))) {
-      Deno.stdout.writeSync(encode("error: input is not a number\n"));
+      Deno.stdout.writeSync(
+        encode(`\u001b[31merror\u001b[0m: '${input}' is not a number\n`),
+      );
       continue;
     }
 
@@ -88,6 +98,11 @@ while (true) {
     data = getFileData(filePath);
     balance = getBalance(data);
 
-    printBannerMsg(balance, Number(input) > 0 ? `Added +${input}€ to the balance` : `Removed ${input}€ from the balance`);
+    printBannerMsg(
+      balance,
+      Number(input) > 0
+        ? `Added +${input}€ to the balance`
+        : `Removed ${input}€ from the balance`,
+    );
   }
 }
--- utils.ts
+++ utils.ts
@@ -2,42 +2,56 @@ const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
 export function encode(x: string) {
-    return encoder.encode(x);
+  return encoder.encode(x);
 }
 export function decode(x: BufferSource) {
-    return decoder.decode(x);
+  return decoder.decode(x);
 }
 
 export function getFileData(file: string | URL) {
-    return Deno.readTextFileSync(file);
+  return Deno.readTextFileSync(file);
 }
 
 export function formatDate(date: Date) {
-    return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() +
-        "." + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() +
+    "." + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
 
 export function getBalance(data: string) {
-    return Math.round((data.split("\n").map((x) =>
-                                Number(x.substring(x.indexOf("]")).replace("]", "").replace("\r", ""))
-                               ).reduce((a, b) => {
-                                   return a + b;
-                               }, 0) + Number.EPSILON) * 100 ) / 100;
+  return Math.round(
+    (data.split("\n").map((x) => {
+      const substring = x.substring(x.indexOf("]")).replace("]", "").replace(
+        "\r",
+        "",
+      );
+      return Number(substring);
+    }).reduce((a, b) => {
+      return a + b;
+    }, 0) + Number.EPSILON) * 100,
+  ) / 100;
 }
 
 export function printBannerMsg(balance: number, message?: string) {
-    Deno.stdout.writeSync(encode("\u001bc"));
-    Deno.stdout.writeSync(encode(` ------------------------------ MON v0.0.3 ------------------------------ \n`));
-    Deno.stdout.writeSync(
-        encode(
-            "  Quick help: \u001b[32m<h>\u001b[0m:History \u001b[32m<c>\u001b[0m: Clear history \u001b[32m<R>\u001b[0m:Reset balance \u001b[32m<q>\u001b[0m:Quit\n",
-        ),
-    );
-    Deno.stdout.writeSync(encode(`  Balance: ${balance}€\n`));
+  Deno.stdout.writeSync(encode("\u001bc"));
+  Deno.stdout.writeSync(
+    encode(
+      ` ------------------------------ MON v0.0.3 ------------------------------ \n`,
+    ),
+  );
+  Deno.stdout.writeSync(
+    encode(
+      "  Quick help: \u001b[32m<h>\u001b[0m:History \u001b[32m<c>\u001b[0m: Clear history \u001b[32m<R>\u001b[0m:Reset balance \u001b[32m<q>\u001b[0m:Quit\n",
+    ),
+  );
+  Deno.stdout.writeSync(encode(`  Balance: ${balance}€\n`));
 
-    if (message) {
-        Deno.stdout.writeSync(encode(`  \u001b[34mInfo\u001b[0m: ${message}\n`))
-    }
+  if (message) {
+    Deno.stdout.writeSync(encode(`  \u001b[34mInfo\u001b[0m: ${message}\n`));
+  }
 
-    Deno.stdout.writeSync(encode(" ------------------------------------------------------------------------ \n"));
+  Deno.stdout.writeSync(
+    encode(
+      " ------------------------------------------------------------------------ \n",
+    ),
+  );
 }
